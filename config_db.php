807f5eb74157f234b7f522ae8a88d2a34d53789d
Julian <japaquin@hotmail.com>
2013-09-22 22:46:39 -0600

    database and config scripts were added

---
+++ config_db.php
@@ -0,0 +1,7 @@
+<?php
+// CONSTANTS USED BY THE SCRIPTS.  CHECK EACH TO SEE IF IT IS NULL. IF SO, SET THE VALUE.
+defined('DB_SERVER') ? null : define("MYSQL_SERVER", "localhost");
+defined('DB_USER') ? null : define("MYSQL_USER", "user");
+defined('DB_PASS') ? null : define("MYSQL_PASS", "password");
+defined('DB_NAME') ? null : define("MYSQL_NAME_OF_DB", "database_name");
+?>
